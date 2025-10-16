ee07bf1edc2827c73701847cfb8e6edbce753ed1
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-10-16 10:32:29 +0000

    Feat: Add custome user models that is staff, customer and manager

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    """"Custom user model with role based access"""
+
+    role_choices = (
+        ('customer', 'Customer'),
+        ('staff', 'Staff'),
+        ('manager', 'Manager'),
+    )
+
+    role = models.CharField(max_length=20, choices=role_choices, default='customer')
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
+    
+    def is_customer(self):
+        return self.role == 'customer'
+    def is_staff(self):
+        return self.role == 'staff'
+    def is_manager(self):
+        return self.role == 'manager'
