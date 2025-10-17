805016b47e48f23e66120143d3d4a6659511affc
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-10-17 10:16:58 +0000

    Feat: Add ccustomer registration view, login view, logout view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from .forms import CustomerRegistrationForms
 
 # Create your views here.
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        form = CustomerRegistrationForms(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful! Welcome to GWCL Complaints Tracker')
+            return redirect('home')
+    else:
+        form = CustomerRegistrationForms()
+
+    return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None: # User is authenticated
+                login(request, user)
+                messages.success(request, f'Eelcome back {username}.')
+                return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, 'You have been logged out successfully')
+    return redirect('home')
