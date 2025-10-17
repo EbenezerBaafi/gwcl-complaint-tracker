34e9dd284711c034ae64a960eac26ffbcf14555a
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-10-17 09:51:53 +0000

    Feat: Add customer registration form

---
+++ users/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class customerRegistrationForms(UserCreationForm):
+    emai = forms.EmailField(required=True)
+    phone_number = forms.CharField(max_length=15, required=True)
+    address = forms.CharField(widget=forms.Textarea, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number', 'address', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'customer'
+        if commit:
+            user.save()
+        return user
