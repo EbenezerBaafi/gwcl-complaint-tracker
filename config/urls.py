5bbd1d117def42f995c0d483df80faa94106d77b
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-10-18 18:21:33 +0000

    made migrations

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    #path('complaints/', include('complaints.urls'))
 ]
 
 if settings.DEBUG:
